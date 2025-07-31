cc07ed3a51df67a999e171df106685d29f4c1733
S7R4NGER3301 <nikolakoew@gmail.com>
2025-07-31 18:39:07 +0300

    adding all nav elements

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -2,6 +2,13 @@ import styles from './nav.module.css'
 
 export default function Nav() {
     return <div className={styles.navContainer}>
-        
+        <h1 className={styles.navHeading}>Belle Studio</h1>
+        <div className={styles.navLinksContainer}>
+            <p className={styles.navLink}>Home</p>
+            <p className={styles.navLink}>Services</p>
+            <p className={styles.navLink}>About</p>
+            <p className={styles.navLink}>Contact</p>
+        </div>
+        <button className={styles.navButton}>Book Now</button>
     </div>
 }
