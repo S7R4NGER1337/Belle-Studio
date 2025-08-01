71cac8a6f9b7892b01dc1a33c9c160fdeab26b31
S7R4NGER3301 <nikolakoew@gmail.com>
2025-08-01 17:14:37 +0300

    finishing are you ready section

---
+++ client/src/components/AreYouReady.js
@@ -0,0 +1,15 @@
+import styles from './areYouReady.module.css'
+
+export default function AreYouReady({sectionData}) {    
+    const sectionHeading = sectionData.heading
+    const buttonName = sectionData.buttonName
+    const sectionParagraph = sectionData.sectionParagraph
+
+    return(
+        <div className={styles.sectionContainer}>
+            <h1 className={styles.sectionHeading}>{sectionHeading}</h1>
+            {sectionParagraph && <p className={styles.sectionParagraph}>{sectionParagraph}</p>}
+            <button className={styles.sectionButton}>{buttonName}</button>
+        </div>
+    )
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,16 @@
+import AreYouReady from "../components/AreYouReady";
 import HeroSection from "../components/HeroSection";
 import OurServices from "../components/OurServices";
 
-export default function Home(params) {
+export default function Home() {
+    const readyForYourJourney = {
+        heading: 'Ready for Your Beauty Journey?',
+        buttonName: 'Book Your Session'
+    }
+    
     return <>
         <HeroSection />
         <OurServices />
+        <AreYouReady sectionData={readyForYourJourney}/>
     </>
 }
